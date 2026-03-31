e99ced91aa23d7be0d1437776a08fced4c697c10
anthonny0803 <anthonny0803@gmail.com>
2026-03-31 10:51:22 +0200

    Add scheduled job to auto-complete reservations past end time

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Jobs\AutoCompleteReservationsJob;
 use App\Jobs\SendReservationRemindersJob;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::job(new SendReservationRemindersJob())->everyThirtyMinutes();
+Schedule::job(new AutoCompleteReservationsJob())->everyFifteenMinutes();
